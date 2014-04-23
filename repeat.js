636a573846743cebc626e4c58c742c8f6d821dbe
Howard Tang <tangh@google.com>
2014-04-23 09:14:48 +0100

    Allows an iterator to be passed to populate the array.

--- repeat.js
+++ repeat.js
@@ -1,10 +1,13 @@
 
-module.exports = function(n, element) {
-	if (n === 0)
-		return [];
+var isFunction = function(e) {
+	return Object.prototype.toString.call(e) === '[object Function]';
+};
 
+module.exports = function(n, element) {
+	if (n === 0) return [];
 	var result = [];
-	for (var i = 0; i < n; i++)
-		result.push(element);
+	for (var i = 0; i < n; i++) {
+		result.push(isFunction(element) ? element(i) : element);
+	}
 	return result;
 };
--- test.js
+++ test.js
@@ -2,11 +2,16 @@ var test = require('tape');
 var repeat = require('./repeat');
 
 test('returned array length should match passed n parameter', function(t) {
-	t.plan(1);
 	t.equal(repeat(10, 0).length, 10);
+	t.end();
 });
 
 test('returned array elements, should match passed element parameter', function(t) {
-	t.plan(1);
 	t.equal(repeat(1, -10)[0], -10);
+	t.end();
+});
+
+test('returned array elements should match result of passed function, when passed i', function(t) {
+	t.equal(repeat(10, function(i) {return i;})[9], 9);
+	t.end();
 });
